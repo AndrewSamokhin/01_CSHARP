43f09a7be446b90835de29cb46de5fa3bdb85647
yari <yari.reduk@gmail.com>
2024-02-24 12:05:24 +0300

    Example Fibbonachi numbers

--- Seminar7 Recursion/Task1/Program.cs
+++ Seminar7 Recursion/Task1/Program.cs
@@ -1,4 +1,6 @@
-﻿int Fibb(int i)
+﻿// Расчет числа Фиббоначи
+/*
+int Fibb(int i)
 {
     //1, 1, 2, 3, 5, 8
     if (i == 1 || i == 2)
@@ -9,21 +11,27 @@
 
 }
 
-for(int = 1; int < 100; i++)
+//Console.WriteLine(Fibb(4));
+
+for(int i = 1; i < 100; i++)
 {
-    Console.WriteLine(Fibb(4));
+    Console.WriteLine($"Fibb ({i}) = {Fibb(i)}");
 }
+*/
 
 
-int size = 100:
+int size = 100;
 int[] fibbs = new int[size];
-Fibb[0] = 1;
-Fibb[1] = 1;
+
+fibbs[0] = 1;
+fibbs[1] = 1;
 
 for (int i = 0; i < 100; i++)
 {
     if (i >= 2)
     {
-        fibbs[i] = fibbs[i - 1] + fibbs[i - 2]
+        fibbs[i] = fibbs[i - 1] + fibbs[i - 2];
     }
+    Console.WriteLine($"Fibb ({i}) = {fibbs[i]}");
 }
+
