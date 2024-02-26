2a204a03f34416535947f4bda68219f9f00b4f78
yari <yari.reduk@gmail.com>
2024-02-26 11:50:52 +0300

    How to find min in massive

---
+++ Seminar3 Massives/Examples1/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = {1, 2, 3};
+
+int min = array[0];
+
+    // int min = int.MaxValue; для определения минимума надо взять 
+    // максимально возможное в int
+
+foreach (var item in array)
+{
+    //  min = item < min ? item : min;  Оператор if else 
+    //  тут записано условие, при выполнении присвоить item, если нет, то оставить min 
+    //  после "?" возвращается  true, после ":" возвращается false.
+    //  min = item < min ? item : min; 
+    // 
+
+    if (item < min)   
+    {
+        min = item;
+    }
+}
+System.Console.WriteLine(min);
