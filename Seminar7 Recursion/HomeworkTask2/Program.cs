a4e8154e2017d339095f4ccb7639b335618714f9
yari <yari.reduk@gmail.com>
2024-03-03 07:56:45 +0300

    Seminar7: Recursion homework task2.

---
+++ Seminar7 Recursion/HomeworkTask2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 2: Напишите программу вычисления функции
+Аккермана с помощью рекурсии. Даны два
+неотрицательных числа m и n. 
+Пример: m = 2, n = 3 -> A(m,n) = 9*/
+
+
+
+int A(int n, int m)
+    {
+        if (n < 0 || m < 0) throw new ArgumentOutOfRangeException();
+        if (n == 0) 
+        {
+            return m + 1;
+        }
+        if (m == 0) 
+        {
+            return A(n - 1, 1);
+        }
+        return A(n - 1, A(n, m - 1));
+    }
+
+int m = 3;
+int n = 3;
+int Akk = A(m, n);
+       Console.WriteLine(Akk);
